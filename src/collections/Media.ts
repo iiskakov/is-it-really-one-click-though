8b08f23b5cf6eb5c9c69f4773128215ed8d6d0e6
Iskander <tradeshells@gmail.com>
2024-09-12 08:19:07 +0500

    limits set up is unavailable collection-wide

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -4,10 +4,6 @@ export const Media: CollectionConfig = {
   slug: 'media',
   upload: {
     mimeTypes: ['image/*', 'video/*'], 
-    limits: {
-      fileSize: 100000000, // 100MB, written in bytes
-    },
-
   },
   access: {
     read: () => true,
