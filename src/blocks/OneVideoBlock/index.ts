1648054f4286fe0a8de9263a48db1b447262ee6f
Iskander <tradeshells@gmail.com>
2024-10-22 18:44:16 +0500

    ok test

--- src/blocks/OneVideoBlock/index.ts
+++ src/blocks/OneVideoBlock/index.ts
@@ -5,12 +5,6 @@ import { Block, Field } from 'payload'
 export const OneVideoBlock: Block = {
   slug: 'OneVideo',
   fields: [
-     {
-      name: 'video',
-      type: 'upload',
-      relationTo: 'media',
-      required: false,
-    },
     {
       name: 'vimeoURL',
       type: 'text',
