ef233d1a42c1227e4a37c5d3c9a76fb0060bd628
Iskander <tradeshells@gmail.com>
2024-12-10 09:44:46 +0500

    h-svh

--- src/components/Intro/Video.js
+++ src/components/Intro/Video.js
@@ -5,7 +5,7 @@ export default function Videos({ videoUrl }) {
     
         <video
           src={videoUrl}
-          className={`  absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-50 ease opacity-100 pointer-events-auto`}
+          className={`  absolute top-0 left-0 w-full h-svh md:h-full object-cover transition-opacity duration-50 ease opacity-100 pointer-events-auto`}
           autoPlay
           playsInline
           style={{ scale: 1.2}}
