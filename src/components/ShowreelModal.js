d7bee5dc2b9876faaef58910deb36281c36d1c16
Iskander <tradeshells@gmail.com>
2024-12-10 09:46:42 +0500

    cross

--- src/components/ShowreelModal.js
+++ src/components/ShowreelModal.js
@@ -63,7 +63,16 @@ const ShowreelModal = ({ url }) => {
                 onClick={handleClose}
                 className="brightness-200 absolute top-4 right-4 text-white text-6xl"
               >
-                ✖
+                <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+  </svg>
               </button>
             </div>
           </div>,
