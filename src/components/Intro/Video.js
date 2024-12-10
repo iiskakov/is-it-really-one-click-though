53ad8b6b71cce4a426e7c03f95f248a670964df6
Iskander <tradeshells@gmail.com>
2024-12-10 09:44:12 +0500

    playsinline

--- src/components/Intro/Video.js
+++ src/components/Intro/Video.js
@@ -7,6 +7,7 @@ export default function Videos({ videoUrl }) {
           src={videoUrl}
           className={`  absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-50 ease opacity-100 pointer-events-auto`}
           autoPlay
+          playsInline
           style={{ scale: 1.2}}
           muted
           loop
