d236e8a301bec414e462f39ad1bcf06a5a345215
Iskander <tradeshells@gmail.com>
2024-12-10 10:00:45 +0500

    try to useeffect the vid

--- src/components/Intro/Video.js
+++ src/components/Intro/Video.js
@@ -1,17 +1,28 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useRef } from 'react';
 
 export default function Videos({ videoUrl }) {
-  return (
-    
-        <video
-          src={videoUrl}
-          className={`  absolute top-0 left-0 w-full h-svh md:h-full object-cover transition-opacity duration-50 ease opacity-100 pointer-events-auto`}
-          autoPlay
-          playsInline
-          style={{ scale: 1.2}}
-          muted
-          loop
-        />
-  );
-};
+  const videoRef = useRef(null);
 
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video && video.paused) {
+      video.play().catch((err) => {
+        console.error("Failed to autoplay video:", err);
+      });
+    }
+  }, []);
+
+  return (
+    <video
+      ref={videoRef}
+      src={videoUrl}
+      className="absolute top-0 left-0 w-full h-svh md:h-full object-cover transition-opacity duration-50 ease opacity-100 pointer-events-auto"
+      autoPlay
+      playsInline
+      muted
+      loop
+      style={{ scale: 1.2 }}
+    />
+  );
+}
